ad62be2d7e9cc328e320c7edd279d09714a0b89d
angelahuqing <angelahuqing@gmail.com>
2024-02-14 11:43:17 -0800

    [SERV-1018] Created basic testing
    Updated main.go to be testable and included running test in GA workflow

--- main.go
+++ main.go
@@ -9,14 +9,33 @@ import (
 
 const Port = 8080
 
+// App represents your application.
+type App struct {
+	Router *gin.Engine
+}
+
 func main() {
+	app := NewApp()
+	app.Routes()
+	app.Router.Run(":" + strconv.Itoa(Port))
+}
+
+// NewApp initializes a new instance of your application.
+func NewApp() *App {
 	r := gin.Default()
+	return &App{Router: r}
+}
 
-	r.GET("/", helloWorld)
-
-	r.Run(":" + strconv.Itoa(Port))
+// Run starts the server.
+func (app *App) Routes() {
+	app.Router.GET("/", helloWorld)
+	app.Router.NoRoute(app.NotFoundHandler)
 }
 
 func helloWorld(c *gin.Context) {
 	c.String(http.StatusOK, "hello world")
+}
+
+func (app *App) NotFoundHandler(c *gin.Context) {
+	c.String(http.StatusNotFound, "This is not yet supported")
 }
